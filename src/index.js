eb8fc768a1027d94357a91ad486e3dcf460b3a84
narekchomoyan <narekchomoyan@gmail.com>
2021-06-04 16:19:47 +0400

    Final Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+      <Auth0Provider
+          domain="spreadsheet.us.auth0.com"
+          clientId="l4tPYaB8omqQXMhewoLOJpTFLJ5VRaE5"
+          audience="https://api.narekchomoyan.com"
+          redirectUri={window.location.origin}>
+                <App />
+      </Auth0Provider>,
   document.getElementById('root')
 );
 
